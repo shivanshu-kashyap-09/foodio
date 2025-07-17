6c8926be372b71f01c8d8d0afb1ad5f5c235d164
unknown <kashyapshivanshu63@gmail.com>
2025-07-17 14:32:46 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import Restaurant from "./Pages/Restaurant";
